c85011ba0072977127b6e331b9ccefcdcb1bd922
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-15 22:18:14 +0500

    test merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
+import java.util.Arrays;
 public class Main {
+    public static void main(String[] args){
+        int[] numbers = {5, 2 ,8 , 1, 3};
+        MergeSorter sorter = new MergeSorter();
+        sorter.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
 }
