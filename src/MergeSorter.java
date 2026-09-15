e1f6b172bb7c3fca42e0ebc210b6674188917a6b
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-15 22:34:04 +0500

    add reusable buffer

--- src/MergeSorter.java
+++ src/MergeSorter.java
@@ -1,21 +1,21 @@
 public class MergeSorter {
     public void sort(int[] array){
-        mergeSort(array, 0 , array.length - 1);
+        int[] temp = new int[array.length];
+        mergeSort(array, temp , 0 , array.length - 1);
     }
-    private void mergeSort(int[]  array , int left, int right){
+    private void mergeSort(int[]  array , int [] temp , int left, int right){
         if (left >= right){
             return;
         }
         int middle = (left + right) / 2;
-        mergeSort(array , left , middle);
-        mergeSort(array , middle + 1 , right);
-        merge(array , left , middle , right);
+        mergeSort(array , temp , left , middle);
+        mergeSort(array , temp ,middle + 1 , right);
+        merge(array , temp , left , middle , right);
     }
-    private void merge(int[] array , int left , int middle , int right){
-        int[] temp = new int[right - left + 1];
+    private void merge(int[] array , int[] temp , int left , int middle , int right){
         int i = left;
         int j = middle + 1;
-        int k = 0;
+        int k = left;
         while (i<= middle && j<= right){
             if(array[i] < array[j]){
                 temp[k] = array[i];
@@ -36,8 +36,8 @@ public class MergeSorter {
             j++;
             k++;
         }
-        for (int x =0; x < temp.length; x++){
-            array[left +x] = temp[x];
+        for (int x =left; x <=right; x++){
+            array[x] = temp[x];
         }
     }
 }
