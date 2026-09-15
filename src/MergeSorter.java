65b1b9948c1b894a13653f415bafa9daa3730347
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-15 23:24:42 +0500

    add metrics

--- src/MergeSorter.java
+++ src/MergeSorter.java
@@ -1,15 +1,27 @@
 public class MergeSorter {
+    public static final int cutoff = 10;
+    private int comparisons = 0;
+    private int maxDepth = 0;
     public void sort(int[] array){
+        comparisons = 0;
+        maxDepth = 0;
         int[] temp = new int[array.length];
-        mergeSort(array, temp , 0 , array.length - 1);
+        mergeSort(array, temp , 0 , array.length - 1, 1);
     }
-    private void mergeSort(int[]  array , int [] temp , int left, int right){
+    private void mergeSort(int[]  array , int [] temp , int left, int right , int depth){
+        if(depth > maxDepth){
+            maxDepth = depth;
+        }
         if (left >= right){
             return;
         }
+        if(right - left + 1 <= cutoff){
+            insertionSort(array, left ,right);
+            return;
+        }
         int middle = (left + right) / 2;
-        mergeSort(array , temp , left , middle);
-        mergeSort(array , temp ,middle + 1 , right);
+        mergeSort(array , temp , left , middle , depth + 1);
+        mergeSort(array , temp ,middle + 1 , right , depth +1);
         merge(array , temp , left , middle , right);
     }
     private void merge(int[] array , int[] temp , int left , int middle , int right){
@@ -17,7 +29,8 @@ public class MergeSorter {
         int j = middle + 1;
         int k = left;
         while (i<= middle && j<= right){
-            if(array[i] < array[j]){
+            comparisons++;
+            if(array[i] <= array[j]){
                 temp[k] = array[i];
                 i++;
             }else {
@@ -39,5 +52,26 @@ public class MergeSorter {
         for (int x =left; x <=right; x++){
             array[x] = temp[x];
         }
+    }
+    private void insertionSort(int[] array , int left , int right){
+        for (int i = left + 1; i <= right; i++){
+            int key = array[i];
+            int j = i - 1;
+            while (j>=left ){
+                comparisons ++;
+                if (array[j] <= key){
+                    break;
+                }
+                array[j + 1] = array[j];
+                j --;
+            }
+            array[j+1] = key;
+        }
+    }
+    public int getComparisons(){
+        return comparisons;
+    }
+    public int getMaxDepth(){
+        return maxDepth;
     }
 }
