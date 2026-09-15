35a515ccd763f0c49a2d81c7e028945d88504ecd
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-15 23:37:37 +0500

    add comparisons and depth

--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,25 +1,38 @@
 import java.util.Random;
 public class QuickSorter {
     Random random = new Random();
+    private int comparisons = 0;
+    private int maxDepth = 0;
     public void sort(int[] array){
-        quickSort(array, 0 , array.length - 1);
+        comparisons = 0;
+        maxDepth = 0;
+        quickSort(array, 0 , array.length - 1 , 1);
     }
-    private void quickSort(int[] array , int left ,int right){
-        if(left>= right){
-            return;
+    private void quickSort(int[] array , int left ,int right , int depth){
+        if (depth > maxDepth){
+            maxDepth = depth;
         }
-        int randomIndex = left + random.nextInt(right - left + 1);
-        int temp = array[randomIndex];
-        array[randomIndex] = array[right];
-        array[right] = temp;
-        int pivotIndex = partition(array , left ,right);
-        quickSort(array , left , pivotIndex - 1);
-        quickSort(array , pivotIndex + 1 , right);
+        while (left < right){
+            int randomIndex = left + random.nextInt(right - left + 1);
+            int temp = array[randomIndex];
+            array[randomIndex] = array[right];
+            array[right] = temp;
+            int pivotIndex = partition(array, left , right);
+            if(pivotIndex - left < right - pivotIndex){
+                quickSort(array, left, pivotIndex - 1 , depth +1);
+                left = pivotIndex + 1;
+            }
+            else{
+                quickSort(array , pivotIndex + 1 , right , depth + 1);
+                right = pivotIndex - 1;
+            }
+        }
     }
     private int partition(int[] array , int left , int right){
         int pivot = array[right];
         int i = left;
         for(int j = left; j<right; j++){
+            comparisons ++;
             if(array[j] < pivot){
                 int temp = array[i];
                 array[i] = array[j];
@@ -31,5 +44,11 @@ public class QuickSorter {
         array[i] = array[right];
         array[right] = temp;
         return i;
+    }
+    public int getComparisons(){
+        return comparisons;
+    }
+    public int getMaxDepth(){
+        return maxDepth;
     }
 }
