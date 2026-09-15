94042add466e77b39bed1bea14e2015afb4540dc
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-15 23:04:06 +0500

    test Quicksort

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
         int[] numbers = {5, 2 ,8 , 1, 3};
         MergeSorter sorter = new MergeSorter();
         sorter.sort(numbers);
+        System.out.println("MergeSort:");
         System.out.println(Arrays.toString(numbers));
+
+        int[] numbers2 = {5 , 2, 8 , 1, 3};
+        QuickSorter quickSorter = new QuickSorter();
+        quickSorter.sort(numbers2);
+        System.out.println("QuickSort:");
+        System.out.println(Arrays.toString(numbers2));
     }
 }
