cf18fb2480e6b6ece2b56a48baec8c303baa8628
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-20 13:28:25 +0500

    add comparisons and recursion depth

--- src/DeterministicSelector.java
+++ src/DeterministicSelector.java
@@ -1,25 +1,33 @@
 import java.util.Arrays;
 public class DeterministicSelector {
+    private int comparisons = 0;
+    private int maxDepth = 0;
     public int select(int[] array , int k){
-        return select(array, 0 , array.length - 1, k);
+        comparisons = 0;
+        maxDepth = 0;
+        return select(array, 0 , array.length - 1, k , 1);
     }
-    private int select(int[] array , int left , int right , int k){
+    private int select(int[] array , int left , int right , int k , int depth){
+        if(depth > maxDepth){
+            maxDepth = depth;
+        }
+
         if (left == right){
             return array[left];
         }
-        int pivot = getPivot(array , left, right);
+        int pivot = getPivot(array , left, right , depth);
         int pivotIndex = partition(array , left , right , pivot);
         if (k== pivotIndex){
             return array[k];
         }
         if (k < pivotIndex){
-            return select(array , left , pivotIndex - 1, k);
+            return select(array , left , pivotIndex - 1, k , depth + 1);
         }
         else {
-            return select(array , pivotIndex + 1 , right , k);
+            return select(array , pivotIndex + 1 , right , k , depth + 1);
         }
     }
-    private int getPivot(int[] array , int left , int right){
+    private int getPivot(int[] array , int left , int right , int depth){
         int size = right - left + 1;
         if(size <= 5){
             Arrays.sort(array , left , right + 1);
@@ -35,11 +43,12 @@ public class DeterministicSelector {
             array[medianIndex] = temp;
             medianCount++;
         }
-        return select(array , left, left + medianCount - 1, left + medianCount/2);
+        return select(array , left, left + medianCount - 1, left + medianCount/2 , depth + 1);
     }
     private int partition(int[] array , int left , int right , int pivot){
         int pivotIndex = left;
         for (int i = left ; i<=right ; i++){
+            comparisons++;
             if(array[i] == pivot){
                 pivotIndex = i;
                 break;
@@ -50,6 +59,7 @@ public class DeterministicSelector {
         array[right] = temp;
         int i = left;
         for (int j = left; j < right; j++){
+            comparisons++;
             if (array[j] < pivot){
                 temp = array[i];
                 array[i] = array[j];
@@ -61,5 +71,11 @@ public class DeterministicSelector {
         array[i] = array[right];
         array[right] =temp;
         return i;
+    }
+    public int getComparisons(){
+        return comparisons;
+    }
+    public int getMaxDepth(){
+        return maxDepth;
     }
 }
