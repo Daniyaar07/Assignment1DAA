34ec14ff44a525f68785f3d5be37ca9fb11c0f8d
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-20 14:24:20 +0500

    add closest pair test and deterministic selector

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,26 @@ public class Main {
         System.out.println(Arrays.toString(numbers2));
         System.out.println("Comparisons:" + quickSorter.getComparisons());
         System.out.println("Max depth:" + quickSorter.getMaxDepth());
+
+        int[] numbers3= {7 , 2 ,9 , 1 , 3};
+        DeterministicSelector selector = new DeterministicSelector();
+        int result = selector.select(numbers3, 2);
+        System.out.println("Deterministic Select:");
+        System.out.println("Result: " + result);
+        System.out.println("Comparisons: " + selector.getComparisons());
+        System.out.println("Max Depth: " + selector.getMaxDepth());
+
+        Point[] points = {
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(5 ,5),
+                new Point(8 , 8)
+        };
+        ClosestPairSolver closestPair = new ClosestPairSolver();
+        double closestDistance = closestPair.findClosest(points);
+        System.out.println("Closest Pair:");
+        System.out.println("Distance: " + closestDistance);
+        System.out.println("Comparisons: " + closestPair.getComparisons());
+        System.out.println("Max depth: " + closestPair.getMaxDepth());
     }
 }
