abbf6dee8898eab4f9d4d10f70c53ff382ca01ce
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-20 15:02:18 +0500

    add timing for deterministic selector and closest pair

--- src/Experiment.java
+++ src/Experiment.java
@@ -31,6 +31,38 @@ public class Experiment {
             System.out.println("QuickSort time: " + time);
             System.out.println("Comparisons: " + quickSorter.getComparisons());
             System.out.println("Max depth: " + quickSorter.getMaxDepth());
+
+            int[] array3 = new int[size];
+            for(int i = 0; i < size; i++){
+                array3[i] = random.nextInt(10000);
+            }
+            DeterministicSelector selector = new DeterministicSelector();
+            int k = size / 2;
+            long startSelect = System.nanoTime();
+            int result = selector.select(array3 , k);
+            long selectTime = System.nanoTime() - startSelect;
+
+            System.out.println("Deterministic Select time: " + selectTime);
+            System.out.println("Result: " + result);
+            System.out.println("Comparisons: " + selector.getComparisons());
+            System.out.println("Max Depth: " + selector.getMaxDepth());
+
+
+            Point[] points = new Point[size];
+            for(int i = 0; i < size; i++){
+                points[i] = new Point(random.nextInt(10000), random.nextInt(10000) );
+            }
+            ClosestPairSolver closestPair = new ClosestPairSolver();
+            long startClosest = System.nanoTime();
+            double closestResult = closestPair.findClosest(points);
+            long closestTime = System.nanoTime() - startClosest;
+
+            System.out.println("Closest Pair time: " + closestTime );
+            System.out.println("Distance: " + closestResult);
+            System.out.println("Comparisons: " + closestPair.getComparisons());
+            System.out.println("Max Depth: " + closestPair.getMaxDepth());
+
+            System.out.println("--------------------------");
         }
     }
 }
